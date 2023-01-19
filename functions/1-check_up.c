45a8bf9b8531001e128a405ec584ba67eb8af352
Themba Ndlondlo <96048335+Shaix33@users.noreply.github.com>
2023-01-19 11:48:24 +0200

    Delete functions directory

--- functions/1-check_up.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-/**
- * check_up - checks for an uppercase character
- * description - a function that checks for a uppercase character
-
--- functions/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void sum_of_integers(int n, int x);
-int _putchar(char c);
-
-#endif
